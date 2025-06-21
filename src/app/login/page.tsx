a596b66334320b36b6c7bc5a953a70078adb40f9
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-21 23:13:14 +0200

    Firebase auth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/app/lib/firebase'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const handleGoogleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+      router.push('/');
+    } 
+    catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } 
+      else {
+        setError('An unknown error occurred');
+      }
+    }
+  }
+
+  return (
+    <div className="min-h-screen mx-auto py-20">
+      <h2 className="text-2xl font-bold mb-6 text-center">Login to Watch Quest</h2>
+
+      {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+
+      <button
+        onClick={handleGoogleSignIn}
+        className="w-full bg-[var(--color-accent)] text-white p-2 rounded font-semibold cursor-pointer hover:bg-[var(--color-highlight)] transition-colors"
+      >
+        Sign In with Google
+      </button>
+    </div>
+  )
+}
