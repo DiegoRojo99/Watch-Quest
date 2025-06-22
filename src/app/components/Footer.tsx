240e154dbe195ab9ea5929b9fa56c26e37c66ba2
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-22 12:22:41 +0200

    Added TMDB attribution

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,7 @@
 // import Link from "next/link";
 
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="w-full bg-blue-800 text-white">
@@ -8,6 +10,18 @@ export default function Footer() {
           &copy; {new Date().getFullYear()} Watch Quest. All rights reserved.
         </div>
         <div className="flex gap-4">
+          <p>
+            All data is sourced from 
+            <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">
+              <Image
+                src="TMDB.svg"
+                alt="TMDB Logo"
+                width={300}
+                height={25}
+                className="inline-block ml-2 w-24 h-auto"
+              />
+            </a>
+          </p>
           {/* <Link href="/privacy" className="hover:underline">Privacy Policy</Link>
           <Link href="/terms" className="hover:underline">Terms of Service</Link>
           <Link href="https://github.com/diegorojo99/watch-quest" target="_blank" rel="noopener noreferrer" className="hover:underline">
