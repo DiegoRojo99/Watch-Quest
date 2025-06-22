1e1d2b41149584aa28514e66b019c71cbae30c62
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-22 14:47:42 +0200

    Movie data is pulled in endpoint

--- src/types/WatchedMovie.d.ts
+++ src/types/WatchedMovie.d.ts
@@ -1,17 +1,12 @@
 export interface WatchedMovieInput {
   movieId: number;
-  movieTitle: string;
-  moviePoster: string | null;
-  movieReleaseDate: string | null;
-  movieGenres: string[];
 
   watchedDate: string | null;
   rating: number | null;
   notes?: string;
 
-  runtime: number | null;
-  method?: "cinema" | "platform";
-  platform?: Platform | null;
+  method?: "Cinema" | "Platform";
+  platform?: string | null;
 }
 
 export interface WatchedMovieInsert extends WatchedMovieInput {
