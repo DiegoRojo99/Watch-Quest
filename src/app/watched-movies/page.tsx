4e5b085105e83564e7bd4fefe83dc3ce6af5e3e6
DiegoRojo99 <diego99rojo@gmail.com>
2025-06-24 14:06:42 +0200

    Link added in watched movies

--- src/app/watched-movies/page.tsx
+++ src/app/watched-movies/page.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function WatchedMoviesList() {
   const { user, userLoading } = useAuth();
@@ -55,34 +56,36 @@ export default function WatchedMoviesList() {
   return (
     <ul className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-4 mx-2 my-4">
       {movies.map((movie) => (
-        <li key={movie.movieId} className="flex flex-col items-center h-full">
-          {movie.moviePoster ? (
-            <Image
-              src={`https://image.tmdb.org/t/p/w300${movie.moviePoster}`}
-              alt={movie.movieTitle}
-              className="w-full h-auto rounded"
-              loading="lazy"
-              width={300}
-              height={450}
-            />
-          ) : (
-            <div className="w-20 h-28 bg-gray-700 rounded flex items-center justify-center text-xs text-gray-400">
-              No Image
+        <li key={movie.movieId} className="h-full">
+          <Link href={`/movie/${movie.movieId}`} className="flex flex-col items-center h-full cursor-pointer">
+            {movie.moviePoster ? (
+              <Image
+                src={`https://image.tmdb.org/t/p/w300${movie.moviePoster}`}
+                alt={movie.movieTitle}
+                className="w-full h-auto rounded"
+                loading="lazy"
+                width={300}
+                height={450}
+              />
+            ) : (
+              <div className="w-20 h-28 bg-gray-700 rounded flex items-center justify-center text-xs text-gray-400">
+                No Image
+              </div>
+            )}
+            <div className="text-center w-full border p-2 rounded h-full 
+            flex flex-col justify-between space-y-1">
+              <h3 className="font-semibold">{movie.movieTitle}</h3>
+              {movie.watchedDate && (
+                <p className="text-sm text-gray-400">
+                  <FontAwesomeIcon icon={faEye} className="mr-1" />
+                  {new Date(movie.watchedDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+                </p>
+              )}
+              {movie.rating !== undefined && (
+                <p className="text-sm text-yellow-400">{movie.rating}/10</p>
+              )}
             </div>
-          )}
-          <div className="text-center w-full border p-2 rounded h-full 
-          flex flex-col justify-between space-y-1">
-            <h3 className="font-semibold">{movie.movieTitle}</h3>
-            {movie.watchedDate && (
-              <p className="text-sm text-gray-400">
-                <FontAwesomeIcon icon={faEye} className="mr-1" />
-                {new Date(movie.watchedDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
-              </p>
-            )}
-            {movie.rating !== undefined && (
-              <p className="text-sm text-yellow-400">{movie.rating}/10</p>
-            )}
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
